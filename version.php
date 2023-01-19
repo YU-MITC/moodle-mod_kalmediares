e731c573f412b5bb57a849a4b416dd9778ea676f
Media and Information Technology Center <31058667+YU-MITC@users.noreply.github.com>
2023-01-19 22:43:42 +0900

    Update version.php

--- version.php
+++ version.php
@@ -28,13 +28,13 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_kalmediares';
-$plugin->version = 2023011601;
-$plugin->release = '2.1.0BETA2';
-$plugin->maturity = MATURITY_BETA;
+$plugin->version = 2023011900;
+$plugin->release = '2.1.0';
+$plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2022041900;
 $plugin->cron = 0;
 $plugin->supported = [400, 401];
 $plugin->dependencies = array(
-    'local_yukaltura' => 2023011600,
-    'local_yumymedia' => 2023011600
+    'local_yukaltura' => 2023011900,
+    'local_yumymedia' => 2023011900
 );
