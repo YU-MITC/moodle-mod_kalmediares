2d171c6e78051e3824c94bffc85f0185f5347334
Center for Information Infrastructure <gh-cc@mlex.cc.yamaguchi-u.ac.jp>
2025-01-02 21:20:59 +0900

    Fix function comment

--- amd/src/playtrigger.js
+++ amd/src/playtrigger.js
@@ -16,7 +16,7 @@
 /**
  * Scripts for mod_kalmediares
  *
- * @copyright (C) 2016-2023 Yamaguchi University (gh-cc@mlex.cc.yamaguchi-u.ac.jp)
+ * @copyright (C) 2016-2025 Yamaguchi University (gh-cc@mlex.cc.yamaguchi-u.ac.jp)
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
